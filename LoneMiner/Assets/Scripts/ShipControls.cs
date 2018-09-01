7f10b622384fd86c3c695058cd7f163da96725fc
Jdave <davidhabinsky@gmail.com>
2018-08-31 17:05:03 -0700

    feature/foundationstart
    This starts off the player and control mechanics of the game in Unity.

---
+++ LoneMiner/Assets/Scripts/ShipControls.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControls : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float thrust;
+    public float rotateThrust;
+    private float thrustInput;
+    private float rotateInput;
+    Vector3 m_EulerAngleVelocity;
+
+    // Use this for initialization
+    void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Check for input.
+        thrustInput = Input.GetAxis("Vertical");
+        rotateInput = Input.GetAxis("Horizontal");
+        m_EulerAngleVelocity = new Vector3(0, rotateInput * rotateThrust, 0);
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddRelativeForce(Vector3.forward * thrustInput * thrust);
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+   
+
+}
