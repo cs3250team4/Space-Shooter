68f7819f1fcd79774af7fe7fcf2fea71fdecc4ca
Ken Berry <rosskberry@gmail.com>
2018-09-05 20:17:00 -0600

    BasicFollowCam Script
    A basic follow camera

---
+++ LoneMiner/Assets/Scripts/BasicFollowCam.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicFollowCam : MonoBehaviour
+{
+    private GameObject player; // reference to the player game object
+    private Vector3 offset; // offset distance between the player and camera
+
+    // initialization
+    void Start()
+    {
+        // get Player object in scene
+        player = GameObject.Find("Player");
+
+        // Calculate and store the offset value by getting the distance
+        // between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as
+        // the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;
+    }
+}
