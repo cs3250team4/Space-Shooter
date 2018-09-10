6dd3f46d847a129f0e0cb7bde0d518c4ee3ade84
Ken Berry <rosskberry@gmail.com>
2018-09-09 18:59:35 -0600

    Added Prefab, Scripts, & Scene for Ship Thrusters
    Added new "Player" prefab that uses new ThrusterTrigger scripts to trigger thruster particle systems on and off based on user input.  Can be tested out in new Test scene.

---
+++ LoneMiner/Assets/Scripts/ThrusterTriggerLeft.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * ThrusterTriggerLeft
+ * 
+ * Toggles on and off the thruster particles on players
+ * ship when the ship is being rotated to the left.
+ * 
+ * Needs to be added as a component to all auxiliary thruster 
+ * GameObjects that should be on when the ship rotates left.
+ * 
+ * Author(s):   Kenneth Berry
+ */
+
+using UnityEngine;
+
+public class ThrusterTriggerLeft : MonoBehaviour
+{
+
+    private ParticleSystem particles; // thruster ParticleSystem
+
+    /*
+     * Initialization
+     */
+    void Start()
+    {
+        // Get this GameObjects ParticleSystem
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    /*
+     * Update is called once per frame
+     */
+    void Update()
+    {
+        // Check if there is rotate-left input
+        if (Input.GetAxis("Horizontal") < 0.0f)
+        {
+            // Turn on particle system
+            particles.Play();
+        }
+        else
+        {
+            // Turn off particle system
+            particles.Stop();
+        }
+    }
+}
