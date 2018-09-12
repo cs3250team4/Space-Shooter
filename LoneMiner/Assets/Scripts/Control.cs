2939eb0cdd2f4354fd5af41327d4a63decca24b1
Joe Turner <joe.turner@live.com>
2018-09-12 08:51:19 -0600

    Created Parallax Background
    Basic setup for parallax scrolling

---
+++ LoneMiner/Assets/Scripts/Control.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    private float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0) * Time.deltaTime);
+	}
+}
---
+++ LoneMiner/Assets/Scripts/Follow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    public float parallax;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        Material mat = mr.material;
+        Vector2 offset = mat.mainTextureOffset;
+        offset.x = transform.position.x / transform.localScale.x / parallax;
+        offset.y = transform.position.y / transform.localScale.y / parallax;
+        mat.mainTextureOffset = offset;
+    }
+}
