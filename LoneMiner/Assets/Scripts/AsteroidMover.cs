f39cb31294f2261e528f7f96e6186694e08073d2
Joe Turner <joe.turner@live.com>
2018-09-17 08:45:50 -0600

    Working on Mission
    Space Invaders style
    Implemented enemy lasers

---
+++ LoneMiner/Assets/Scripts/AsteroidMover.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Original code by Adam Buckner https://www.linkedin.com/in/adam-buckner-vfx
+ * From Unity Tutorial:
+ * https://unity3d.com/learn/tutorials/projects/space-shooter-tutorial/explosions?playlist=17147
+ * 
+ * Updated by Joe Turner mailto:jturne48@msudenver.edu
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMover : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
