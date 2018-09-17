b8a5a67ed7aed8846cd1005d0df8607c6ac4e379
Unknown <cs3250team4@gmail.com>
2018-09-17 17:29:45 -0600

    Feature/MainMenu
    Menu development

---
+++ LoneMiner/Assets/Scripts/StartButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+    public void LoadByIndex (int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
