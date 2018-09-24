6e2f837b724b30ce1c98c7635c6123fdb3afac0e
Joe Turner <35381823+jet76@users.noreply.github.com>
2018-09-23 18:06:40 -0600

    Mission Controller for Test Mission
    This Script Spawns Waves of Enemies

---
+++ LoneMiner/Assets/Scripts/MissionController.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Orginal code by Adam Buckner: https://www.linkedin.com/in/adam-buckner-vfx
+ * From Unity Tutorial:
+ * https://unity3d.com/learn/tutorials/projects/space-shooter-tutorial/extending-space-shooter-enemies-more-hazards?playlist=17147
+ * 
+ * Modifieded by Joe Turner mailto:jturne48@msudenver.edu
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionController : MonoBehaviour {
+
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+
+}
