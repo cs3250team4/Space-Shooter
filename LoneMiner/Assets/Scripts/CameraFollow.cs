873e21e1e5911c0f516cc18b93d1b86283fe1e9e
David Habinsky <dhabinsk@msudenver.edu>
2018-09-24 17:19:50 -0600

    Ship navigate Sphere foundation
    Sets the camera in position to always look down on ship as it navigates around a sphere.

--- LoneMiner/Assets/Scripts/CameraFollow.cs
+++ LoneMiner/Assets/Scripts/CameraFollow.cs
@@ -3,23 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
- * author: Asbjorn Thirslund
+ * authors: Asbjorn Thirslund (tutorial), David Habinsky
  * 
  */
 
 public class CameraFollow : MonoBehaviour {
 
-    public Transform target;
+
+    public Transform target;  // for positioning camera based  on ship position using empty game object as child to player.
+    public Transform looking; // another transform just to look at the ship or player directly.
 
     public float smoothSpeed = 10f;
     public Vector3 offset;
 
     void FixedUpdate()
     {
+
+        /* potential option of using offset to constantly adjust the camera angle as the ship rotates around sphere.
+        offset.x = target.rotation.y * 2;
+        offset.y = target.rotation.y * 5;
         Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        */
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, target.position, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
 
-        //transform.LookAt(target);
+        transform.LookAt(looking);
     }
 }
---
+++ LoneMiner/Assets/Scripts/GravityBody.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityBody : MonoBehaviour {
+
+    public GravityAttract attractor;
+    private Transform myTransform;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        rb.useGravity = false;
+        myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        attractor.Attract(myTransform);
+	}
+}
