77742dabf5b7bd3281eedfbc769e229c61935114
David Habinsky <dhabinsk@msudenver.edu>
2018-10-21 19:54:36 -0600

    joe Boss new push

---
+++ LoneMiner/Assets/Scripts/DotEAsteroids.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Original code by Adam Buckner https://www.linkedin.com/in/adam-buckner-vfx
+ * From Unity Tutorials:
+ * https://unity3d.com/learn/tutorials/projects/space-shooter-tutorial/creating-hazards?playlist=17147
+ * https://unity3d.com/learn/tutorials/projects/space-shooter-tutorial/explosions?playlist=17147
+ * 
+ * Modified by Joe Turner mailto:jturne48@msudenver.edu
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class DotEAsteroids : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    private DotEMissionController missionController;
+
+    void Start()
+    {
+        GameObject missionControllerObject = GameObject.FindWithTag("GameController");
+        if (missionControllerObject != null)
+        {
+            missionController = missionControllerObject.GetComponent<DotEMissionController>();
+        }
+        if (missionController == null)
+        {
+            Debug.Log("Cannot find 'MissionController' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary" || other.tag == "Enemy" || other.tag == "EnemyShot")
+        {
+            return;
+        }
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+        if (other.tag == "Player")
+        {
+            if (this.tag == "EnemyShot")
+            {
+                PlayerData.control.hullIntegrity -= 1;
+            }
+            if (this.tag == "Enemy")
+            {
+                PlayerData.control.hullIntegrity -= 100;
+            }
+            if (PlayerData.control.hullIntegrity <= 0)
+            {
+                PlayerData.control.hullIntegrity = 0;
+                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+                Destroy(other.gameObject);
+                missionController.MissionFailed();
+            }
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ LoneMiner/Assets/Scripts/SideScrollPlayerController.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Orginal code by Adam Buckner: https://www.linkedin.com/in/adam-buckner-vfx
+ * From Unity Tutorials:
+ * https://unity3d.com/learn/tutorials/projects/space-shooter-tutorial/moving-player?playlist=17147
+ * https://unity3d.com/learn/tutorials/projects/space-shooter/shooting-shots?playlist=17147
+ * https://unity3d.com/learn/tutorials/projects/space-shooter-tutorial/audio?playlist=17147
+ * 
+ * Updated & Modified by Joe Turner mailto:jturne48@msudenver.edu
+ */
+
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class SideScrollBoundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class SideScrollPlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    //public float zPos;
+    public SideScrollBoundary boundary;
+    private Rigidbody rb;
+    private AudioSource audioSource;
+
+    public GameObject shot;
+    public Transform centerLaser;
+    public Transform leftLaser;
+    public Transform rightLaser;
+    public float fireRate;
+
+    private float nextFire;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            if(shot != null)
+            {
+                nextFire = Time.time + fireRate;
+                if (centerLaser != null)
+                {
+                    Instantiate(shot, centerLaser.position, centerLaser.rotation);
+                }                
+                if(leftLaser != null)
+                {
+                    Instantiate(shot, leftLaser.position, leftLaser.rotation);
+                }
+                if(rightLaser != null)
+                {
+                    Instantiate(shot, rightLaser.position, rightLaser.rotation);
+                }                
+                audioSource.Play();
+            }            
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(-moveVertical, 0.0f, moveHorizontal);
+        rb.velocity = movement * speed;
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt + 90.0f);
+    }
+
+}
