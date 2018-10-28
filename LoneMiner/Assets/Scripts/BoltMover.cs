50e67e5a3e5dad77d050e126913848743d79c3cd
David Habinsky <dhabinsk@msudenver.edu>
2018-10-28 13:07:09 -0600

    PixelMake Tutorial done
    Pixel Make tutorial assets will be mixed into mission

--- LoneMiner/Assets/Scripts/BoltMover.cs
+++ LoneMiner/Assets/Scripts/BoltMover.cs
@@ -13,17 +13,28 @@ using UnityEngine;
 public class BoltMover : MonoBehaviour {
 
     public float speed;
+    public float damage;
     private Rigidbody rb;
 
 	// Use this for initialization
 	void Start()
     {
         rb = GetComponent<Rigidbody>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update()
     {
         rb.velocity = transform.forward * speed;
-	}
+        Destroy(gameObject, 2);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Asteroid") ;
+        {
+            collision.transform.GetComponent<PixelMake_AstroidController>().stats.currentHealth -= damage;
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ LoneMiner/Assets/Scripts/PixelMakeScripts/Bullet_Control.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Control : MonoBehaviour {
+
+    public float BulletSpeed;
+    public float damage;
+    private Rigidbody rb;
+
+    void start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rb.velocity = transform.forward * BulletSpeed;
+        Destroy(gameObject, 2);
+    }
+	
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Asteroid");
+        {
+            collision.transform.GetComponent<PixelMake_AstroidController>().stats.currentHealth -= damage;
+            Destroy(gameObject);
+        }
+    }
+        
+
+}
