be4eb3f149b63cb73b3514eff15de3fa495248c7
Unknown <pfennel1@msudenver.edu>
2018-10-31 16:15:17 -0600

    Adding prefab to inventory menue
    In pervious pull request a prefab was missing.

---
+++ LoneMiner/Assets/Scripts/CanvasInPauseMenue.cs
@@ -0,0 +1,45 @@
+﻿/* This script controls the persentation of a menu on a GUI canvas
+ * Use this as a base for your menu.
+ * The hot key is ESC.
+ * Pete Fennell 10-1-2018
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasInPauseMenue : MonoBehaviour
+{
+    public OptionsMenuInventory optionsMenu;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+        optionsMenu.gameObject.SetActive(!optionsMenu.gameObject.activeSelf);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            optionsMenu.gameObject.SetActive(!optionsMenu.gameObject.activeSelf);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+
+                if (Time.timeScale == 1)
+
+                    Time.timeScale = 0;
+                else
+                    Time.timeScale = 1;
+            }
+
+        }
+
+    }
+}
