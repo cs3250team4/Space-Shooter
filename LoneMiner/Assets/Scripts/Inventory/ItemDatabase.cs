561eb277fb703b3fc4ee68500d20d5e09b1616ee
Unknown <saenzjudith@outlook.com>
2018-11-01 22:20:25 -0600

    Inventory Scripts
    11/1 @10:20PM

---
+++ LoneMiner/Assets/Scripts/Inventory/ItemDatabase.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour{
+    public List<Item> items = new List<Item>();
+
+    private void Awake()
+    {
+        BuildDatabase();
+    }
+
+    public Item GetItem(int id)
+    {
+        return items.Find(item => item.id == id);
+    }
+
+    public Item GetItem(string itemName)
+    {
+        return items.Find(item => item.title == itemName);
+    }
+
+    void BuildDatabase()
+    {
+        items = new List<Item>() {
+            new Item(0, "Coal", "A piece of coal.",
+            new Dictionary<string, int>
+            {
+                {"Coin Value", 5}
+            })
+            };
+
+        items = new List<Item>() {
+              new Item(1, "Gold", "A rare piece of gold.",
+              new Dictionary<string, int>
+              {
+                  {"Coin Value", 10}
+              })
+              };
+
+        items = new List<Item>() {
+              new Item(2, "Asteroid", "A chunk of an asteroid.",
+              new Dictionary<string, int>
+              {
+                  {"Coin Value", 2}
+              })
+              };
+
+        items = new List<Item>() {
+              new Item(3, "Scrap Metal", "A piece of scrap metal.",
+              new Dictionary<string, int>
+              {
+                  {"Coin Value", 6}
+              })
+              };
+
+        items = new List<Item>() {
+              new Item(4, "Rare Metal", "A rare piece of metal.",
+              new Dictionary<string, int>
+              {
+                  {"Coin Value", 8}
+              })
+              };
+
+        items = new List<Item>() {
+                new Item(5, "Rare Rock", "A rare piece of rock.",
+                new Dictionary<string, int>
+                {
+                  {"Coin Value", 10}
+                })
+                };
+
+        items = new List<Item>() {
+               new Item(6, "Rock", "A common piece of rock.",
+               new Dictionary<string, int>
+               {
+                  {"Coin Value", 1}
+               })
+               };
+
+
+    }
+}
---
+++ LoneMiner/Assets/Scripts/Inventory/UIInventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventory : MonoBehaviour
+{
+
+    public List<UIItem> uIItems = new List<UIItem>();
+    public GameObject slotPrefab;
+    public Transform slotPanel;
+    public int numberOfSlots = 10;
+
+    private void Awake()
+    {
+        for(int i = 0; i < numberOfSlots; i++)
+        {
+            GameObject instance = Instantiate(slotPrefab);
+            instance.transform.SetParent(slotPanel);
+            uIItems.Add(instance.GetComponentInChildren<uIItem>);
+
+        }
+    }
+public void UpdateSlot(int slot, Item item)
+{
+    uIItems[slot].UpdateItem(item);
+}
+
+public void AddNewItem (Item item)
+{
+    UpdateSlot(UIItems.FindIndex(i => i.item == null), item);
+}
+
+public void RemoveItem(Item item)
+{
+    UpdateSlot(uIItems.FindIndex(i => i.item == item), null);
+}
+}
